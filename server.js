f56aabbb73e566589f2e99b75d8d7912b0e796e8
Tomas Arauz <desarrollo05@biossoft.net>
2021-02-18 13:18:24 -0500

    master

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express=require("express");
-const path=require("path");
-const app=express();
-
-app.use(express.static(__dirname+'/dist/appangular'));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/appangular/index.html'));
-});
-
-app.listen(process.env.PORT || 8888);
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/appangular'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/appangular/index.html'));});
+app.listen(process.env.PORT || 8080);
